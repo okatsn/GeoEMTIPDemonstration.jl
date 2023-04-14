8d22beaefeccb47ae01b50fd61ec3b109a66ad3c
Documenter.jl <documenter@juliadocs.github.io>
2023-04-14 03:02:43 +0000

    build based on 4af79e9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GeoEMTIPDemonstration","category":"page"},{"location":"#GeoEMTIPDemonstration","page":"Home","title":"GeoEMTIPDemonstration","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GeoEMTIPDemonstration.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GeoEMTIPDemonstration]","category":"page"}]
+}
