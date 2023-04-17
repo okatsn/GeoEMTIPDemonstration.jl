98d93492e2d337dc6b1993d57a32f944b82f3d79
okatsn <okatsn@gmail.com>
2023-04-17 10:32:04 +0800

    density and contour plots

--- scripts/CWB2023mid/makie_molchan_plane.jl
+++ scripts/CWB2023mid/makie_molchan_plane.jl
@@ -22,49 +22,18 @@ xymap = mapping(
 )
 molplane_scatter = data(df) * xymap
 
-# scatter plot colored by group *prp*:
+# ### colored by group *prp*:
+# scatter plot 
 set_aog_theme!()
 axis = (width = 225, height = 225)
 molplane_scatter * mapping(color = :prp) |> draw
 
+# contour plot 
+layer_contour = AlgebraOfGraphics.density() * visual(Contour)
+molplane_scatter * layer_contour * mapping(color = :prp) |> draw
 
-molplane_density = molplane_scatter * visual(Contour)
-draw(molplane_density)
-# randguess = 
-
-(data(df) * visual(Contour) + data(df)) * xymap |> draw
-
-
-
-molplane * mapping(color = :prp, marker = :frc) + ablines(1, -1) |> draw
-
-
-ablines(1, -1)
-
-
-
-
-plot(df, x=:AlarmedRateForecasting, y = :MissingRateForecasting, 
-    intercept=[1], slope=[-1], Geom.abline(color="red", style=:dash),
-    Geom.density2d(levels = 5), Geom.point, color = :group1_colors, alpha = [0.1],
-    Coord.cartesian(xmin=0, xmax=1, ymin=0, ymax = 1, aspect_ratio=1)
-)
-
-# ## Each a subplot
-set_default_plot_size(21cm, 8cm)
-plot(df, 
-    intercept=[1], slope=[-1], # args for abline
-    x=:AlarmedRateForecasting, y = :MissingRateForecasting, 
-    xgroup = :prp, color = :group1_colors, alpha = [0.1],
-    Geom.subplot_grid(
-        Geom.density2d(levels = 7), 
-        Geom.point,
-        Geom.abline(color="red", style=:dash),
-        Coord.cartesian(xmin=0, xmax=1, ymin=0, ymax = 1, aspect_ratio=1),
-        # Coord.cartesian(xmin=0, xmax=1, ymin=0, ymax = 1, aspect_ratio=1)
-    )
-)
-
-
-
+# density 3D plot
+layer_wireframe = AlgebraOfGraphics.density() * visual(Wireframe, linewidth = 0.05)
+ax3d = (type = Axis3, width = 300, height = 300)
+molplane_scatter * layer_wireframe * mapping(color = :prp) |> p -> draw(p; axis = ax3d)
 
