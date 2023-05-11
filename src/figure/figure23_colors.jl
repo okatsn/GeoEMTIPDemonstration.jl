a6ae5f9e57cf004184f79e53fec54c08c4493418
okatsn <okatsn@gmail.com>
2023-05-11 17:36:38 +0800

    Move categorical_colors to OkMakieToolkits

--- src/figure/figure23_colors.jl
+++ src/figure/figure23_colors.jl
@@ -4,16 +4,6 @@ mutable struct ColorsFigure23{T<:Union{ColormapRef, Nothing}} <: PlotColors
     trial::T
 end
 
-
-"""
-`categorical_colors(args...; kwargs...)` creates a vector of colors of `RGBA` with by-default `alpha=1.0`; it takes the same `args` and `kwargs` as `CairoMakie.cgrad`.
-"""
-function categorical_colors(args...; alpha=1.0, kwargs...) # alpha is required and it overwrite the ones in kwargs.
-    colorgrad = CairoMakie.cgrad(args...; categorical = true, kwargs...) # to avoid `ERROR: ArgumentError: range(0.0, stop=1.0, length=1): endpoints differ` when the length of colors is 1
-    return identity.(colorgrad.colors) # See  Makie.wong_colors |> methods
-end
-
-
 function ColorsFigure23(P::Prep202304;
                 frccolor = :jet,
                 prpcolor = categorical_colors(:Set1_4, 4), # Makie.wong_colors()
