1c561ca21a699e916e8ce19a4ddd7b28cd703fb0
okatsn <okatsn@gmail.com>
2023-05-11 10:56:35 +0800

    Fix categorical_colors

--- src/figure/figure23_colors.jl
+++ src/figure/figure23_colors.jl
@@ -1,16 +1,16 @@
-
 mutable struct ColorsFigure23{T<:Union{ColormapRef, Nothing}} <: PlotColors
     frc::T
     prp::T
     trial::T
 end
 
+
 """
-`categorical_colors(args...; kwargs...)` creates a vector of colors with by-default `alpha=1.0`; it takes the same `args` and `kwargs` as `CairoMakie.cgrad`.
+`categorical_colors(args...; kwargs...)` creates a vector of colors of `RGBA` with by-default `alpha=1.0`; it takes the same `args` and `kwargs` as `CairoMakie.cgrad`.
 """
 function categorical_colors(args...; alpha=1.0, kwargs...) # alpha is required and it overwrite the ones in kwargs.
-    colors = CairoMakie.cgrad(args...; categorical = true, kwargs...) # to avoid `ERROR: ArgumentError: range(0.0, stop=1.0, length=1): endpoints differ` when the length of colors is 1
-    return RGBAf.(colors, alpha) # See  Makie.wong_colors |> methods
+    colorgrad = CairoMakie.cgrad(args...; categorical = true, kwargs...) # to avoid `ERROR: ArgumentError: range(0.0, stop=1.0, length=1): endpoints differ` when the length of colors is 1
+    return identity.(colorgrad.colors) # See  Makie.wong_colors |> methods
 end
 
 
