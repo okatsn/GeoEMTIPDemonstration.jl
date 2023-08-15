560efe8da6fd647678de84aa03cddcd093e16f6f
okatsn <okatsn@gmail.com>
2023-08-15 15:56:58 +0800

    Load and combine EQK summaries

---
+++ scripts/CWB2023mid/makie_eqk_prb_plot.jl
@@ -0,0 +1,21 @@
+using DataFrames, CSV
+using CairoMakie, AlgebraOfGraphics
+using Statistics
+using LaTeXStrings
+using Printf
+import NaNMath: mean as nanmean
+# using Revise # using Revise through VSCode settings
+using CWBProjectSummaryDatasets
+using GeoEMTIPDemonstration
+using Dates
+using OkFiles
+
+df_ge = CSV.read("data/temp/PhaseTestEQK_GE_3yr_180d_500md_2023J30.csv", DataFrame)
+df_gm = CSV.read("data/temp/PhaseTestEQK_GM_3yr_180d_500md_2023J30.csv", DataFrame)
+df_mix = CSV.read("data/temp/PhaseTestEQK_MIX_3yr_180d_500md_2023J30.csv", DataFrame)
+
+for (tag, df) in zip(["GE", "GM", "MIX"], [df_ge, df_gm, df_mix])
+    insertcols!(df, :trial => tag)
+end
+
+df = vcat(df_ge, df_gm, df_mix)
