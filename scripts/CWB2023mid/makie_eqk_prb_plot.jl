bd53a44585dcd02b9f4c4fa40bcc759d1454ca99
okatsn <okatsn@gmail.com>
2023-08-15 16:53:36 +0800

    Date string/time/integer index conversion

--- scripts/CWB2023mid/makie_eqk_prb_plot.jl
+++ scripts/CWB2023mid/makie_eqk_prb_plot.jl
@@ -7,6 +7,7 @@ import NaNMath: mean as nanmean
 # using Revise # using Revise through VSCode settings
 using CWBProjectSummaryDatasets
 using GeoEMTIPDemonstration
+using OkMakieToolkits
 using Dates
 using OkFiles
 
@@ -14,8 +15,22 @@ df_ge = CSV.read("data/temp/PhaseTestEQK_GE_3yr_180d_500md_2023J30.csv", DataFra
 df_gm = CSV.read("data/temp/PhaseTestEQK_GM_3yr_180d_500md_2023J30.csv", DataFrame)
 df_mix = CSV.read("data/temp/PhaseTestEQK_MIX_3yr_180d_500md_2023J30.csv", DataFrame)
 
-for (tag, df) in zip(["GE", "GM", "MIX"], [df_ge, df_gm, df_mix])
+tagdfs = Dict(
+    "GE" => df_ge,
+    "GM" => df_gm,
+    "MIX" => df_mix
+);
+
+for (tag, df) in tagdfs
     insertcols!(df, :trial => tag)
 end
 
 df = vcat(df_ge, df_gm, df_mix)
+
+# convert `timeStr` to `DateTime`
+transform!(df, :timeStr => ByRow(t -> DateTime(t, "d-u-y")) => :dt)
+
+# full list of datetime; TimeAsX.
+fulldt = df.dt |> unique |> sort
+dictTX = Dict(fulldt .=> eachindex(fulldt))
+transform!(df, :dt => ByRow(t -> dictTX[t]) => :x)
