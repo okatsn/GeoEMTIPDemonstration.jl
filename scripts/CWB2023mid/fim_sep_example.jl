6f78b8bcc49f4e5f8ed12152c6351a0042798691
okatsn <okatsn@gmail.com>
2023-11-06 11:18:34 +0800

    Finishing the plot

--- scripts/CWB2023mid/fim_sep_example.jl
+++ scripts/CWB2023mid/fim_sep_example.jl
@@ -6,7 +6,7 @@ using Statistics
 iszero(v) = v == 0.0
 nanmean(x) = mean(filter(!isnan, x))
 
-df = CSV.read("LargeFiles/delete_after_2025/fimsep_example.csv", DataFrame; header=
+df = CSV.read("LargeFiles/delete_after_2025/stn[CHCH]dt[20210101-20210105]type[GEMS].csv", DataFrame; header=
 [
     :year, :month, :day, :hour, :minute, :second,
     :FIM_1minute_NS,
@@ -21,6 +21,8 @@ df = CSV.read("LargeFiles/delete_after_2025/fimsep_example.csv", DataFrame; head
     :FIM_1day_EW,
     :SE_1day_NS,
     :SE_1day_EW,
+    :signal_15Hz_NS,
+    :signal_15Hz_EW,
 ])
 
 transform!(df,
@@ -37,14 +39,18 @@ df = combine(groupby(df, :time), :time => unique, Not(:time) .=> nanmean; rename
 
 
 
-df2 = stack(df, Cols(r"^FIM", r"^SE"), :time)
+df2 = stack(df, Cols(r"^FIM", r"^SE", r"^signal"), :time)
+
 transform!(df2, :variable => ByRow(str -> begin
     e = split(str, "_")
     (metric=e[1], window=e[2], component=e[3])
 end) => AsTable)
 
+# filter!(:window => (str -> str in ["1hour", "15Hz"]), df2)
+filter!(:value => !isnan, df2)
+
 # dropmissing!(df2)
 
 with_theme(resolution=(1000, 550)) do
-    data(df2) * visual(Lines) * mapping(:time, :value) * mapping(col=:component, row=:metric) * mapping(color=:window) |> p -> draw(p; facet=(; linkyaxes=:rowwise), axis=(; xticklabelrotation=0.2π))
+    data(df2) * visual(Lines, linewidth=1) * mapping(:time, :value) * mapping(col=:component, row=:metric) * mapping(color=:window => sorter((["15Hz", "1minute", "1hour", "1day"]))) |> p -> draw(p; facet=(; linkyaxes=:rowwise), axis=(; xticklabelrotation=0.2π))
 end
