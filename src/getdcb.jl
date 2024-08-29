e7d436596db278ac6fff3e97e36c48645025424a
okatsn <okatsn@gmail.com>
2024-08-29 08:37:53 +0000

    Keyword option: Cache results and apply big.

--- src/getdcb.jl
+++ src/getdcb.jl
@@ -1,23 +1,30 @@
-uniqNEQ = 0:10
-DCB = Dict([α => Dict([neq => molchancb(big(neq), α) for neq in uniqNEQ]) for α in [0.05, 0.1, 0.32]])
-# Expand this dictionary for your needs.
 """
-A dictionary function for efficiently obtain Molchan confidence boundary.
+A dictionary to cache computed binomial coefficients.
 """
-getalms(α, neq) = DCB[α][neq]
+const binomial_cache = Dict{Tuple{Float64,Int},Float64}()
 
 
 """
 `getdcb(α, neq)` calculate alarmed rate and hit rate for the given complementary confidence level `α` and number of earthquakes `neq`.
-Use `big(neq)` instead if `neq > 20`.
 
-Since `molchancb(N, alpha)` for N > 20 is slow because julia is slow in handling BigInt, `getdcb` looks into dictionary first if there are existing results already, and otherwise use `molchancb`.
+`getdcb` applies `big` if `neq` is larger than keyword argument `applybig` (40 by default).
+
+Since julia is slow in handling `BigInt`, `getdcb` by default looks into dictionary `binomial_cache` first if there are cached results already; this behavior is controlled by `cache=true` keyword argument.
 """
-function getdcb(α, neq)
-    (alarmed, missed) = try
-        (alarmed, missed) = getalms(α, neq) # Looks into a dictionary so it will be much faster
-    catch
-        (alarmed, missed) = molchancb(neq, α) # Use `big(neq)` to prevent stack overflow error.
+function getdcb(α, neq; cache=true, applybig=40)
+    if neq > applybig
+        neq = big(neq)  # Use `big(neq)` to prevent stack overflow error.
     end
+
+    if cache && haskey(binomial_cache, (α, neq))
+        return binomial_cache[(α, neq)]
+    end
+
+    (alarmed, missed) = molchancb(neq, α)
     fdcb = 1.0 .- alarmed .- missed
+
+    if cache
+        binomial_cache[(n, k)] = fdcb
+    end
+    return fdcb
 end
